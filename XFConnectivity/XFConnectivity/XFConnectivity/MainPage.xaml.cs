b7d78b2cfc5f28087ee4887654e0a17b3eb287f8
Sebastian Jensen <apps@tsjdev-apps.de>
2021-01-09 13:01:54 +0100

    Add sample for Connectivity API.

---
+++ XFConnectivity/XFConnectivity/XFConnectivity/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFConnectivity
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Connectivity.ConnectivityChanged += OnConnectivityChanged;
+
+            NetworkAccessLabel.Text = Connectivity.NetworkAccess.ToString();
+            ConnectionProfilesLabel.Text = string.Join(", ", Connectivity.ConnectionProfiles);
+        }
+
+        private void OnConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            NetworkAccessLabel.Text = e.NetworkAccess.ToString();
+            ConnectionProfilesLabel.Text = string.Join(", ", e.ConnectionProfiles);
+        }
+    }
+}
