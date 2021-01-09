b576b57ad5d7f4b7ce02f0ae9eb2a544d839b720
Sebastian Jensen <apps@tsjdev-apps.de>
2021-01-09 11:19:05 +0100

    Add sample for Preferences API.

---
+++ XFPreferences/XFPreferences/XFPreferences/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFPreferences
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private readonly string PreferencesKey = "MyPreferencesKey";
+
+        private void SaveValueButtonOnClicked(object sender, EventArgs e)
+        {
+            Preferences.Set(PreferencesKey, PreferencesValueEntry.Text);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            PreferencesValueEntry.Text
+                = Preferences.Get(PreferencesKey, "Default Value");
+        }
+    }
+}
