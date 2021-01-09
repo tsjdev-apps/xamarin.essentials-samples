ec8bb499531ae66bec493487eafd8be10c36b3bb
Sebastian Jensen <apps@tsjdev-apps.de>
2021-01-09 11:10:48 +0100

    Add sample for Browser API.

---
+++ XFBrowser/XFBrowser/XFBrowser/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFBrowser
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenWebsiteButtonOnClicked(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://www.tsjdev-apps.de");
+        }
+    }
+}
