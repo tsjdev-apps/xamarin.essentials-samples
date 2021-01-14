ee72f1cfe5cbf71c4582a8c57248c25bb63fcff5
Sebastian Jensen <apps@tsjdev-apps.de>
2021-01-14 07:38:22 +0100

    Add sample for Screenshot API.

---
+++ XFScreenshot/XFScreenshot/XFScreenshot/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFScreenshot
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ScreenshotButtonOnClicked(object sender, System.EventArgs e)
+        {
+            var screenshot = await Screenshot.CaptureAsync();
+            var stream = await screenshot.OpenReadAsync();
+
+            ScreenshotImage.Source = ImageSource.FromStream(() => stream);
+        }
+    }
+}
