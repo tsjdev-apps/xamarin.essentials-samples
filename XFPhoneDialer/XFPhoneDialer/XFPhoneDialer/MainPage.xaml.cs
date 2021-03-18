e57b879db58f241cc3a1f5b016dd8fc38be2f2bc
Sebastian Jensen <apps@tsjdev-apps.de>
2021-03-18 20:31:26 +0100

    Add sample for PhoneDialer API.

---
+++ XFPhoneDialer/XFPhoneDialer/XFPhoneDialer/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFPhoneDialer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CallButtonOnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open(PhoneNumberEntry.Text);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+    }
+}
