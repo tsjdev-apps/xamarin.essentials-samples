b4a21fe476cfc0a3d4c88e84df1b3f5a0570b057
Sebastian Jensen <apps@tsjdev-apps.de>
2021-04-02 11:12:24 +0200

    Add sample for Clipboard API.

---
+++ XFClipboard/XFClipboard/XFClipboard/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFClipboard
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SetClipboardButtonOnClicked(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(ClipboardEntry.Text);
+        }
+
+        private async void GetClipboardButtonOnClicked(object sender, EventArgs e)
+        {
+            ClipboardLabel.Text = await Clipboard.GetTextAsync();
+        }
+    }
+}
