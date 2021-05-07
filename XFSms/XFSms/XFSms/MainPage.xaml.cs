1a602802b9be3c89df5a57aee24edecc9f4c8d6d
Sebastian Jensen <apps@tsjdev-apps.de>
2021-05-07 07:45:52 +0200

    Add sample for SMS API.

---
+++ XFSms/XFSms/XFSms/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFSms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SMSButtonOnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var message = new SmsMessage(MessageBodyEntry.Text, RecipientEntry.Text);
+                await Sms.ComposeAsync(message);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine($"{GetType().Name} | {nameof(SMSButtonOnClicked)} | {ex}");
+            } 
+        }
+    }
+}
