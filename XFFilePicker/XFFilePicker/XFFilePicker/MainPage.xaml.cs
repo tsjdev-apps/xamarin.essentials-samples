26884023cbc614f79ffec372f4e649401d5c7bb2
Sebastian Jensen <apps@tsjdev-apps.de>
2021-06-17 21:28:10 +0200

    Add sample for FilePicker API.

---
+++ XFFilePicker/XFFilePicker/XFFilePicker/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XFFilePicker
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void PickFileButtonOnClicked(object sender, System.EventArgs e)
+		{
+			try
+			{
+				var options = new PickOptions { PickerTitle = "Pick a file" };
+				var fileResult = await FilePicker.PickAsync(options);
+
+				if (fileResult == null)
+					return;
+
+				FileNameLabel.Text = fileResult.FileName;
+			}
+			catch(Exception ex)
+			{
+				Debug.WriteLine($"{GetType().Name} | {nameof(PickFileButtonOnClicked)} | {ex}");
+			}
+		}
+	}
+}
